c8965e42d358f815eea4e4f97d702dc41f0db2e2
AJ0895 <ar.annjo@gmail.com>
2022-12-09 22:41:45 -0500

    Final Submission FP4

--- Final project/Scripts/One.js
+++ Final project/Scripts/One.js
@@ -35,20 +35,26 @@ const queryString=window.location.search;
 const params= new URLSearchParams(queryString);
 const dog= params.get("pups");
 
+//Changing the Prof name
 document.querySelector(".pupNam").textContent=dog;
 
+//Changing the name
 document.querySelector(".dogName").textContent=dog;
 
+//Changing the profile image
 document.querySelector(".profImage").src="./" +pups[dog].imgFile;
 
+//Changing the breed
 console.log(pups[dog].dbreed);
 document.querySelector(".breed").textContent=pups[dog].dBreed;
 
+//Changing the description
 document.querySelector(".des").textContent=pups[dog].des;
 
+//Changing the story
 document.querySelector(".stor").textContent=pups[dog].story;
 
-console.log("entered");
+//console.log("entered");
 
 
 
--- Final project/Scripts/Store.js
+++ Final project/Scripts/Store.js
@@ -12,8 +12,8 @@ const pups={
         "imgFile":"/Assets/Dog photo series/2.jpg",
         "dBreed":"Indie2",
         "gender": "Male",
-        "des": "Charlie is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you,",
-        "story":"Charlie is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you"
+        "des": "Charlie is a puppy that came to us in the summer. He's three tall and love to play. The puppy had a ear surgery and has reduced hearing. He loves everyone who pats him",
+        "story":"Meet Charlie, who came to us as an owner surrender along with his brother. Charlie has lived with other dogs and cats and gets along great with children ( and chickens and ducks, too!). Full of energy and loves to lick the air. Yup, that's what we said: he licks the air. How fun! As with older dogs of this breed, Charlie needs some attention to his teeth and a member of our medical team would be happy to discuss this with you. Come meet Charlie today at our Northside location!"
     },
     "Tommy":{
         "name":"Tommy",
@@ -21,31 +21,31 @@ const pups={
         "dBreed":"Indie3",
         "gender": "Male",
         "des": "Tommy is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you,",
-        "story":"Tommy is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you"
+        "story":"There may be millions of Tommies in the world, but this one is just for you. Tommy is a senior dog currently at our North Side Campus. He's a very very loving dog who just wants to be your lap dog through her golden years. He has some of the medical conditions a senior dog would have, and a member of our Medical team will be glad to talk to you about her during your visit. If you're looking for a BFF, looking to move into the country, or just looking for an awesome dog, visit Tommy today!"
     },
     "Julie":{
         "name":"Julie",
         "imgFile":"/Assets/Dog photo series/4.jpg",
         "dBreed":"Indie4",
         "gender": "Female",
-        "des": "Julie is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you,",
-        "story":"Julie is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you"
+        "des": "Julie is an affectionate female puppy who adores people and other dogs. She likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you,",
+        "story":"Meet Julie! She was brought in as a stray, so unfortunately not much is known about her past. However, we do know that she's done with her traveling days and ready to find her forever home. Julie has had too many sweets in her past, so now she's going to need some dental work after adoption and does have a heart murmur. Our medical staff would be happy to discuss these with potential adopters"
     },
     "Coco":{
         "name":"Coco",
         "imgFile":"/Assets/Dog photo series/5.jpg",
         "dBreed":"Indie5",
         "gender": "Female",
-        "des": "Henry is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you,",
-        "story":"Henry is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you"
+        "des": "Coco is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you,",
+        "story":"Meet Coco! This sweet and curious boy is looking for his fur-ever home. He came to us as a stray and is a bit nervous for vet visits, which our training and enrichment team would be happy to discuss with you. Come meet this meatball and bring home Mr. Meaty for the holidays!"
     },
     "Scotch":{
         "name":"Scotch",
         "imgFile":"/Assets/Dog photo series/6.jpg",
         "dBreed":"Indie6",
         "gender": "Female",
-        "des": "Henry is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you,",
-        "story":"Henry is an affectionate boy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you"
+        "des": "Scotch is an affectionate female puppy who adores people and other dogs. He likes to swim, and lives for car rides. Henry enjoys hiking and loves to get out and go on adventures. After a long day he is content to cuddle up on the couch and watch TV and snuggle with you,",
+        "story":"Meet Scotch! Scotch is just a little girl in a big scary world! We recommend positive reinforcement obedience training to help this sweetie build up her confidence and be the best dog she can be. She has lived with several of her littermates, and may be open to furry roommates in the future - we just ask that you bring them in to meet her before finalizing her adoption. Come on down and meet this baby today!"
     }
 
 };
